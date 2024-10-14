4a56a1c5617e773966843fd7ec2ca6a21d0a3eb7
Theredhawksings <myeoseul@gmail.com>
2024-10-14 17:15:09 +0900

    import 및 reset world 추가 그리고 끌 수있게 하기

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from pico2d import *
+import random
+import pygame
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+def reset_world():
+    global running
+    running = True
 
 
 
+reset_world()
 open_canvas(800, 600)
 
-while True:
+while running:
+    handle_events()
     delay(0.01)
 
 close_canvas()

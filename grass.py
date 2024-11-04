10b978ef57c92dece04f8fbb8aead196d33a8715
Theredhawksings <myeoseul@gmail.com>
2024-11-04 19:56:33 +0900

    풀 전산화

--- grass.py
+++ grass.py
@@ -2,29 +2,29 @@ from pico2d import load_image
 import os
 
 class Grass:
-    image1 = None
-    image2 = None
-    image3 = None
+   image1 = None
+   image2 = None
+   image3 = None
 
-    def __init__(self, positions, current_stage=1):
-        if Grass.image1 is None:
-            Grass.image1 = load_image(os.path.join('Grass', 'Grass.png'))
-        if Grass.image2 is None:
-            Grass.image2 = load_image(os.path.join('Grass', 'Grass2.png'))
-        if Grass.image3 is None:
-            Grass.image3 = load_image(os.path.join('Grass', 'Grass3-3.png'))
+   def __init__(self, positions, current_stage=1):
+       if Grass.image1 is None:
+           Grass.image1 = load_image(os.path.join('Grass', 'Grass.png'))
+       if Grass.image2 is None:
+           Grass.image2 = load_image(os.path.join('Grass', 'Grass2.png'))
+       if Grass.image3 is None:
+           Grass.image3 = load_image(os.path.join('Grass', 'Grass3-3.png'))
 
-        self.current_stage = current_stage
-        self.positions = positions
+       self.current_stage = current_stage
+       self.positions = positions
 
-    def draw(self):
-        for x, y, width in self.positions:
-            if self.current_stage >= 4:
-                Grass.image3.clip_draw(0, 0, width * 2, 60, x, y, width * 2, 60)
-            elif self.current_stage == 3:
-                Grass.image2.clip_draw(0, 0, width * 2, 60, x, y, width * 2, 60)
-            else:
-                Grass.image1.clip_draw(0, 0, width * 2, 60, x, y, width * 2, 60)
+   def draw(self):
+       for x, y, width in self.positions:
+           if self.current_stage == 4 or self.current_stage == 5:
+               Grass.image3.clip_draw(0, 0, width * 2, 60, x, y, width * 2, 60)
+           elif self.current_stage == 3:
+               Grass.image2.clip_draw(0, 0, width * 2, 60, x, y, width * 2, 60)
+           else:
+               Grass.image1.clip_draw(0, 0, width * 2, 60, x, y, width * 2, 60)
 
-    def get_positions(self):
-        return self.positions
+   def get_positions(self):
+       return self.positions

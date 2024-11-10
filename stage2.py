c9b7019ad805fa434774d39a730f5050bfa7f360
Theredhawksings <myeoseul@gmail.com>
2024-11-10 14:27:13 +0900

    최적화 중

--- stage2.py
+++ stage2.py
@@ -1,10 +1,7 @@
 from pico2d import *
-
-import collision_utils
 from grass import Grass
 from ground import Ground
 from obstacle import Obstacle
-from collision_utils import check_collision, handle_collision, get_death_count
 from Font import Font
 import time
 import random
@@ -21,14 +18,13 @@ class Stage2:
         obstacle_data = [
             (270, 680, 0, 0, 0),
             (400, 680, 0, 0, 0),
-            (460, 755, 2, 0 ,0),
+            (460, 755, 2, 0, 0),
             (600, 755, 2, 0, 0),
             (800, 680, 0, 0, 0),
             (200, 755, 2, 0, 0),
             (520, 680, 0, 0, 0),
             (680, 680, 0, 0, 0),
             (780, 680, 0, 0, 0),
-            #1
 
             (200, 530, 0, 0, 0),
             (200, 606, 2, 0, 0),
@@ -39,7 +35,6 @@ class Stage2:
             (700, 606, 2, 0, 0),
             (780, 530, 0, 0, 0),
             (1000, 530, 0, 0, 0),
-            #2
 
             (30, 360, 0, 0, 0),
             (55, 360, 0, 0, 0),
@@ -58,15 +53,11 @@ class Stage2:
             (810, 456, 2, 0, 0),
             (890, 360, 0, 0, 0),
             (925, 330, 0, 0, 0),
-            #3
         ]
-        
-        self.obstacle = Obstacle(obstacle_data)
 
+        self.obstacle = Obstacle(obstacle_data)
         self.stage_change_call = stage_change_call
         self.boy.update_stage_info(2)
-
-
         self.font = Font(30)
 
     def handle_event(self, event):
@@ -78,7 +69,7 @@ class Stage2:
 
         current_time = time.time()
 
-        if current_time % 0.05 <= 0.01 and self.boy.y < 300 and self.boy.x>=315:#좀 느리게
+        if current_time % 0.05 <= 0.01 and self.boy.y < 300 and self.boy.x >= 315:
             new_obstacle = {
                 'x': random.randint(350, 900),
                 'y': 300,
@@ -92,23 +83,18 @@ class Stage2:
             self.boy.x = 1020
             self.boy.y = 80
             self.stage_change_call(1)
-
         elif self.boy.x > 1024:
             self.boy.x = 1024
-
         elif self.boy.x <= 280 and self.boy.y < 0:
             self.stage_change_call(3)
 
-        for obstacle in self.obstacle.obstacles:
-            if check_collision(self.boy, obstacle['x'], obstacle['y'], obstacle['image_direction']):
-                self.boy.savepointX = 67
-                self.boy.savepointY = 700
-                handle_collision(self.boy)
-                break
+        self.boy.savepointX = 67
+        self.boy.savepointY = 700
+        self.obstacle.check_collision(self.boy)
 
     def draw(self):
         self.ground.draw(512, 384)
         self.grass.draw()
         self.boy.draw()
         self.obstacle.draw()
-        self.font.draw(30, 750, f"죽은 횟수: {collision_utils.get_death_count()}", (0, 0, 0))
+        self.font.draw(30, 750, f"죽은 횟수: {Obstacle.get_death_count()}", (0, 0, 0))

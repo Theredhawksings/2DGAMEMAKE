6f191aa53ef862a0f91af139d49e1b0412a09809
Theredhawksings <myeoseul@gmail.com>
2024-11-13 11:16:52 +0900

    물리 물리

--- cyclicobstacle.py
+++ cyclicobstacle.py
@@ -2,12 +2,10 @@ from pico2d import load_image, draw_rectangle
 import math, os
 import time
 
-
 class CyclicObstacle:
     def __init__(self, x, y, image_direction, move_direction, move_speed, active_time=3.0, inactive_time=2.0):
         self.image = load_image(os.path.join('obstacle', 'obstacle.png'))
         self.angles = [0, math.pi / 2, math.pi, 3 * math.pi / 2]
-
         self.x = x
         self.y = y
         self.original_x = x
@@ -15,19 +13,16 @@ class CyclicObstacle:
         self.image_direction = image_direction
         self.move_direction = move_direction
         self.move_speed = move_speed
-
         self.active_time = active_time
         self.inactive_time = inactive_time
         self.start_time = time.time()
         self.is_active = True
-
         self.death_count = 0
 
     def update(self):
         current_time = time.time()
         cycle_time = self.active_time + self.inactive_time
         elapsed_time = (current_time - self.start_time) % cycle_time
-
         self.is_active = elapsed_time < self.active_time
 
         if self.is_active:
@@ -52,22 +47,26 @@ class CyclicObstacle:
                                            self.x,
                                            self.y,
                                            25, 30)
+            self.draw_collision_box()
+
+    def draw_collision_box(self):
+        left, bottom, right, top = self.get_bb()
+        draw_rectangle(left, bottom, right, top)
 
     def get_bb(self):
         if not self.is_active:
             return None
 
         if self.image_direction == 0 or self.image_direction == 2:
-            left = self.x + 15
-            right = self.x + 17
-            bottom = self.y
-            top = self.y + 20
-
+            left = self.x - 10
+            right = self.x + 10
+            bottom = self.y - 15
+            top = self.y + 10
         else:
-            left = self.x
-            right = self.x + 25
-            bottom = self.y + 10
-            top = self.y + 20
+            left = self.x - 15
+            right = self.x + 15
+            bottom = self.y - 15
+            top = self.y + 15
 
         return left, bottom, right, top
 
@@ -100,5 +99,4 @@ class CyclicObstacle:
         )
 
         self.death_count += 1
-
         return True

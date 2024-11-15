029ddd3832cebee5282a67ddb69cdec3739aa249
Theredhawksings <myeoseul@gmail.com>
2024-11-15 13:31:24 +0900

    스테이지 4 수정

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from game_world import GameWorld
 def main():
     open_canvas(1024, 768)
     game_world = GameWorld()
-    game_world.change_stage(4)
+    game_world.change_stage(1)
     game_world.boy.x = 15
     game_world.boy.y = 100
 
--- stage4.py
+++ stage4.py
@@ -70,8 +70,6 @@ class Stage4:
             'obstacle': {'x': -10, 'y': 565, 'image_direction': 3, 'move_direction': 1, 'move_speed': 10}},
            {'trigger': {'x_min': 300, 'x_max': 320, 'y_min': 600, 'y_max': None},
             'obstacle': {'x': 1030, 'y': 580, 'image_direction': 1, 'move_direction': 2, 'move_speed': 30}},
-           {'trigger': {'x_min': 180, 'x_max': 210, 'y_min': 600, 'y_max': None},
-            'obstacle': {'x': 1030, 'y': 580, 'image_direction': 1, 'move_direction': 2, 'move_speed': 30}},
            {'trigger': {'x_min': 180, 'x_max': 800, 'y_min': 720, 'y_max': None},
             'obstacle': {'x': 20, 'y': 740, 'image_direction': 3, 'move_direction': 1, 'move_speed': 5}}
        ]

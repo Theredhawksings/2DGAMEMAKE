e9aa8acb7488daebd3b48bd734f37a95ad833017
Theredhawksings <myeoseul@gmail.com>
2024-11-19 03:28:21 +0900

    땅 추가 브금 추가

--- game_world.py
+++ game_world.py
@@ -1,17 +1,40 @@
 import os
+import time
 import pygame
 from pico2d import *
 from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
 
-import stage1
-import stage2
-import stage3
-import stage4
-import stage5
-import stage6
-import stage7
-
 from boy import Boy, IdleState
+from stage1 import Stage1
+from stage2 import Stage2
+from stage3 import Stage3
+from stage4 import Stage4
+from stage5 import Stage5
+from stage6 import Stage6
+from stage7 import Stage7
+
+class MusicManager:
+    def __init__(self):
+        self.current_music = None
+        pygame.mixer.init()
+
+    def load_music(self, music_path):
+        if self.current_music != music_path:
+            if self.current_music is not None:
+                pygame.mixer.music.stop()
+            pygame.mixer.music.load(music_path)
+            pygame.mixer.music.play(-1)
+            self.current_music = music_path
+
+    def stop_music(self):
+        pygame.mixer.music.stop()
+        self.current_music = None
+
+    def pause_music(self):
+        pygame.mixer.music.pause()
+
+    def unpause_music(self):
+        pygame.mixer.music.unpause()
 
 
 class GameWorld:
@@ -21,14 +44,38 @@ class GameWorld:
         self.last_stage = 1
         self.boy = Boy()
         self.boy.game_world = self
-        self.current_music = None
         self.state = 'LOGO'
         self.logo_time = 0.0
         self.logo_image = load_image(os.path.join('screen', '3570144307.png'))
         self.intro_image = load_image(os.path.join('screen', 'Intro.png'))
-        pygame.mixer.init()
-        pygame.mixer.music.load(os.path.join('bgm', 'Eagle_sing.mp3'))
-        pygame.mixer.music.play(-1)
+        self.stage_change_time = None
+        self.music_manager = MusicManager()
+
+        self.music_manager.load_music(os.path.join('bgm', 'Eagle_sing.mp3'))
+
+    def change_stage(self, stage_number):
+        stage_classes = [
+            None,
+            Stage1,
+            Stage2,
+            Stage3,
+            Stage4,
+            Stage5,
+            Stage6,
+            Stage7,
+        ]
+
+        if 1 <= stage_number <= 7:
+            stage_class = stage_classes[stage_number]
+            self.current_stage = stage_class(self.change_stage, self.boy)
+            self.stage_change_time = time.time()
+
+        self.last_stage = stage_number
+
+        if stage_number == 7:
+            self.music_manager.stop_music()  # 10초 후에 재생하기 위해 일단 중지
+        else:
+            self.load_music(stage_number)
 
     def load_music(self, stage_number):
         if stage_number in [1, 2, 3]:
@@ -38,31 +85,14 @@ class GameWorld:
         else:
             return
 
-        if self.current_music != music_path:
-            if self.current_music is not None:
-                pygame.mixer.music.stop()
-            pygame.mixer.music.load(music_path)
-            pygame.mixer.music.play(-1)
-            self.current_music = music_path
+        self.music_manager.load_music(music_path)
 
-    def change_stage(self, stage_number):
-        stage_classes = [
-            None,
-            stage1.Stage1,
-            stage2.Stage2,
-            stage3.Stage3,
-            stage4.Stage4,
-            stage5.Stage5,
-            stage6.Stage6,
-            stage7.Stage7,
-        ]
-
-        if 1 <= stage_number <= 7:
-            stage_class = stage_classes[stage_number]
-            self.current_stage = stage_class(self.change_stage, self.boy)
-
-        self.last_stage = stage_number
-        self.load_music(stage_number)
+    def load_stage7_music_after_delay(self):
+        if self.current_stage and isinstance(self.current_stage, Stage7):
+            current_time = time.time()
+            if current_time - self.stage_change_time >= 10:  # 10초 후에 재생
+                music_path = os.path.join('bgm', 'madara.mp3')
+                self.music_manager.load_music(music_path)
 
     def handle_events(self):
         events = get_events()
@@ -73,14 +103,14 @@ class GameWorld:
                 if event.key == SDLK_ESCAPE:
                     if self.state == 'PLAY':
                         self.state = 'PAUSE'
-                        pygame.mixer.music.pause()
+                        self.music_manager.pause_music()
                         self.boy.key_states['left'] = False
                         self.boy.key_states['right'] = False
                         self.boy.event_queue.clear()
                         self.boy.state_machine.start(IdleState)
                     elif self.state == 'PAUSE':
                         self.state = 'PLAY'
-                        pygame.mixer.music.unpause()
+                        self.music_manager.unpause_music()
                     else:
                         self.running = False
                     continue
@@ -101,11 +131,11 @@ class GameWorld:
             self.logo_time += 0.01
             if self.logo_time >= 2.0:
                 self.state = 'INTRO'
-                pygame.mixer.music.stop()
-                pygame.mixer.music.load(os.path.join('bgm', "102. Lucas' Theme.mp3"))
-                pygame.mixer.music.play(-1)
+                self.music_manager.stop_music()
+                self.music_manager.load_music(os.path.join('bgm', "102. Lucas' Theme.mp3"))
         elif self.state == 'PLAY':
             self.current_stage.update()
+            self.load_stage7_music_after_delay()
 
     def draw(self):
         clear_canvas()
@@ -113,6 +143,6 @@ class GameWorld:
             self.logo_image.draw(512, 384)
         elif self.state == 'INTRO':
             self.intro_image.draw(512, 384)
-        elif self.state == 'PLAY' or self.state == 'PAUSE':
+        elif self.state in ['PLAY', 'PAUSE']:
             self.current_stage.draw()
         update_canvas()

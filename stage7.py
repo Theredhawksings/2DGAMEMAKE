4ed4cfb13530b6afb2c4c0defde3c7f306a9234a
Theredhawksings <myeoseul@gmail.com>
2024-11-20 14:19:35 +0900

    스테이지7 수정

--- stage7.py
+++ stage7.py
@@ -3,8 +3,9 @@ from boss import Boss
 from grass import Grass
 from ground import Ground
 import collision_utils
+from obstacle import BossObstacle, OBSTACLE_SPEED_PPS
 import time
-
+from random import randint
 
 class Stage7:
     def __init__(self, stage_change_call, boy):
@@ -13,21 +14,28 @@ class Stage7:
         self.start_time = time.time()
         self.boss_activated = False
 
+        self.boss_obstacle = BossObstacle([])
+        self.last_obstacle_time = time.time()
+
+        #collision_utils.add_collision_pair('boy:boss_obstacle', [self.boy], [self.boss_obstacle])
+
         grass_positions = [
-            (20, 0, 120),
-            (20, 150, 120),
-            (20, 300, 120),
-            (20, 450, 120),
-            (20, 600, 120),
+            (20, 0, 180),
+            (20, 150, 180),
+            (20, 300, 180),
+            (20, 450, 180),
+            (20, 600, 180),
         ]
 
         self.grass = Grass(grass_positions, current_stage=7)
+
         self.ground = Ground(current_stage=7)
         self.stage_change_call = stage_change_call
         self.bullets = []
         self.boss = Boss()
         self.boss.boy = boy
         self.boy.update_stage_info(7)
+        self.boss_obstacle.boss = self.boss
 
         self.boss_blood_image = load_image(os.path.join('image', 'boss blood.png'))
 
@@ -64,19 +72,39 @@ class Stage7:
             if bullet in self.bullets:
                 self.bullets.remove(bullet)
 
+        if self.boss_activated and not self.boss.dead:
+            current_time = time.time()
+            if current_time - self.last_obstacle_time >= 3.0:
+                self.boss_obstacle.obstacles.append({
+                    'x': randint(50, 180),
+                    'y': 780,
+                    'move_speed': OBSTACLE_SPEED_PPS
+                })
+                self.last_obstacle_time = current_time
+
+        self.boss_obstacle.update()
+
     def draw(self):
-        self.ground.draw(512, 384)
+        if not self.boss_activated:
+            self.ground.draw(512, 384)
+        else:
+            self.ground.update_stage(8)
+            self.ground.draw(512, 384)
+
         self.grass.draw()
         self.boy.draw()
-
-        if self.boss.dead:
-            self.grass = Grass([(512, 0, 512)], current_stage=7)
 
         if self.boss_activated:
             self.boss.draw()
             health_ratio = max(self.boss.health / 200, 0)
             width = int(1024 * health_ratio)
-            self.boss_blood_image.clip_draw(0, 0, width, 15, 1024 - width / 2, 753, width, 15)
+            x = 1024 - (1024 - width)  
+            self.boss_blood_image.clip_draw(0, 0, width, 15, x / 2, 753, width, 15)
+
+        if self.boss.dead:
+            self.grass = Grass([(512, 0, 512)], current_stage=7)
 
         for bullet in self.bullets:
             bullet.draw()
+
+        self.boss_obstacle.draw()
